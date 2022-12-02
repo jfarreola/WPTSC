c686675299826d85e8a273be5b2ba833a5fdd1c7
JARREOLA <JARREOLA@L-BHCDGB3.wins.lcra.org>
2022-12-02 10:43:42 -0600

    Initial version

---
+++ Test Suites/Basic.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>720d2a4c-0228-4e61-b2aa-fedb3d5b0e48</testSuiteGuid>
+   <testCaseLink>
+      <guid>226395a1-c77a-44c7-bce1-b3fe90f535c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DEV_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
